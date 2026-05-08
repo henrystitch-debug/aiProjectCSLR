f918841eccba5e963f49f77252dc80235485364f
CSLR Team <aiprojectyzu@gmail.com>
2026-05-08 13:45:20 +0800

    commit changes

--- CorrNet_Plus_CSLR/modules/resnet.py
+++ CorrNet_Plus_CSLR/modules/resnet.py
@@ -100,7 +100,7 @@ class Temporal_weighting(nn.Module):
         return x*(F.sigmoid(out.unsqueeze(-1).unsqueeze(-1))-0.5) * self.alpha
 
 class Get_Correlation(nn.Module):
-    def __init__(self, channels, neighbors=3, agg_mode='weighted'):
+    def __init__(self, channels, neighbors=1, agg_mode='weighted'):
         super().__init__()
         reduction_channel = channels//16
 
